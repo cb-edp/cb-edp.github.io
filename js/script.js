3704da1506a9ea1b0b8c6c4bde2f3c48de184a1a
kevvr <rombouts.kevin@gmail.com>
2020-05-27 16:37:42 +0200

    Fixed menu

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var menu = document.getElementsByClassName("menu-button");
 var i;
 
 for (i=0;i < menu.length; i++){
- menu [i].addEventListener("click", function () { 
+ menu[i].addEventListener("click", function() {
   this.classList.toggle("active");
-  var items = this.nextEelementSibling;
+  var items = this.nextElementSibling;
   if (items.style.display === "block") {
    items.style.display = "none";
   } else {
