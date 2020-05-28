d455706992df85107b830b6fba7c1c6c0b16d4a8
kevvr <rombouts.kevin@gmail.com>
2020-05-28 15:23:43 +0200

    Fixed dynamic menu

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-// Menu
-var menu = document.getElementsByClassName("menu-button");
-var i;
+// Load menu
+var oReq = new XMLHttpRequest();
+var sidebar = document.querySelector('#sidebar');
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "/nav.html");
+oReq.send();
 
-for (i=0;i < menu.length; i++){
- menu[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var items = this.nextElementSibling;
-  if (items.style.display === "block") {
-   items.style.display = "none";
-  } else {
-   items.style.display = "block";
-  }
- });
+function reqListener () {
+ sidebar.innerHTML = this.responseText;
+ // Menu
+ var menu = document.getElementsByClassName("menu-button");
+ var i;
+
+ for (i=0;i < menu.length; i++){
+  menu[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var items = this.nextElementSibling;
+   if (items.style.display === "block") {
+    items.style.display = "none";
+   } else {
+    items.style.display = "block";
+   }
+  });
+ }
 }
 
 // Dynamic input
-const input = document.querySelector('input,textarea');
-const log = document.getElementById('element_introductions_v1_area1_output');
+// const input = document.querySelector('input,textarea');
+// const log = document.getElementById('element_introductions_v1_area1_output');
 
-input.addEventListener('input', updateValue);
+// input.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
+// function updateValue(e) {
+//   log.textContent = e.target.value;
+// }
 
 // Dynamic image
-var loadFile = function(event) {
-	var image = document.getElementById('element_introductions_v1_image_output');
-	image.src = URL.createObjectURL(event.target.files[0]);
-};
+// var loadFile = function(event) {
+// 	var image = document.getElementById('element_introductions_v1_image_output');
+// 	image.src = URL.createObjectURL(event.target.files[0]);
+// };
