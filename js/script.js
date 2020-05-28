2ee7f25cb37c4eaf4976a6b2313e5fd9c5001145
kevvr <rombouts.kevin@gmail.com>
2020-05-28 13:01:41 +0200

    Created embed for HTML

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Menu
 var menu = document.getElementsByClassName("menu-button");
 var i;
 
@@ -12,3 +13,19 @@ for (i=0;i < menu.length; i++){
   }
  });
 }
+
+// Dynamic input
+const input = document.querySelector('input,textarea');
+const output = document.getElementById('element_introductions_v1_area1_output');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  output.textContent = e.target.value;
+}
+
+// Dynamic image
+var loadFile = function(event) {
+	var image = document.getElementById('element_introductions_v1_image_output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
